6196464529e140f83bad917863b8c191e2f79606
joseph <joe@cs.brown.edu>
2010-03-08 12:08:33 -0500

    [adsafe] Add an option to ADSafe main to read from lambdaJS strings.
    The option --ljs will assume that the input string is lambdaJS, and
    use that parser.  The --no-env argument is assumed when --ljs is used.

--- ADsafe/src/ADsafe.hs
+++ ADsafe/src/ADsafe.hs
@@ -13,6 +13,7 @@ import BrownPLT.JavaScript.ADsafe.Transformation
 import qualified BrownPLT.JavaScript.ADsafe.DisableBanned as B
 import qualified BrownPLT.JavaScript.ADsafe.DisableEval as E
 import qualified BrownPLT.JavaScript.ADsafe.DisableWindow as W
+import qualified BrownPLT.JavaScript.ADsafe.MakeableTags as K
 import BrownPLT.JavaScript.ADsafe.SimplifyIf
 import BrownPLT.JavaScript.Parser ( parseScriptFromString )
 import BrownPLT.JavaScript.Semantics.Desugar ( desugar )
@@ -21,26 +22,40 @@ import BrownPLT.JavaScript.Semantics.PrettyPrint ( pretty, prettyANF )
 import BrownPLT.JavaScript.Semantics.Syntax
 import BrownPLT.JavaScript.Semantics.ANF
 import BrownPLT.JavaScript.Semantics.RemoveHOAS
-
+import BrownPLT.JavaScript.Semantics.Parser as P
+import BrownPLT.JavaScript.Analysis.RemoveUnused
+import BrownPLT.JavaScript.Analysis.PushDown
 
 desugarMain     opts = mainTemplate pretty opts
 desugarANFMain  opts = mainTemplate (prettyANF . adsafeANF) opts
 getCheckMain    opts = mainTemplateError B.isTypeable opts
 evalCheckMain   opts = mainTemplateError E.isTypeable opts
 windowCheckMain opts = mainTemplateError (W.isTypeable . adsafeANF) opts
+tagCheckMain    opts = mainTemplateError (K.isTypeable . adsafeANF) opts
 
 mainTemplate :: (ExprPos -> String) -> [Flag] -> IO ()
-mainTemplate fn opts = do
-  str <- getContents
-  case parseScriptFromString "<stdin>" str of
-    Right script -> 
-      let env = envForDesugar opts 
-        in do putStrLn $ liftJS fn env script
-              exitSuccess
-    Left err -> do
-      putStrLn $ show err
-      exitFailure
-
+mainTemplate fn opts = 
+    case opts of
+      opts | null [() | LJS <- opts] -> do
+                str <- getContents
+                case parseScriptFromString "<stdin>" str of
+                  Right script -> 
+                      let env = envForDesugar opts 
+                      in do putStrLn $ liftJS fn env script
+                            exitSuccess
+                  Left err -> do
+                    putStrLn $ show err
+                    exitFailure
+      otherwise -> do
+                str <- getContents
+                case P.parseExprFromString "<stdin>" str of
+                  Right script -> 
+                      do putStrLn $ fn script
+                         exitSuccess
+                  Left err -> do
+                     putStrLn $ show err
+                     exitFailure
+                
 mainTemplateShow :: Show a => (ExprPos -> a) -> [Flag] -> IO ()
 mainTemplateShow f = mainTemplate (show . f)
 
@@ -56,7 +71,7 @@ adsafeDesugar script env =
       core4 = rewriteErrors core3
     in core4
 
-adsafeANF = ifReduce . exprToANF
+adsafeANF =  {-pushDown . removeUnused . ifReduce . removeUnused . ifReduce .-} exprToANF
 
 envForDesugar :: [Flag] -> ExprPos -> ExprPos
 envForDesugar opts | null [() | NoEnv <- opts] = ecma262Env
@@ -65,17 +80,20 @@ envForDesugar opts | null [() | NoEnv <- opts] = ecma262Env
 data Flag
   = Action ([Flag] -> IO ())
   | TypeCheck ([Flag] -> IO ())
+  | LJS
   | NoEnv
 
 data Checkers
   = Get
   | Eval
   | Window
+  | Tags
 
 readChecker = Action . readChecker' . (map toLower)
   where readChecker' "get"    = getCheckMain
         readChecker' "eval"   = evalCheckMain
         readChecker' "window" = windowCheckMain
+        readChecker' "tags"   = tagCheckMain
         readChecker' _        = undefined
 
 options :: [OptDescr Flag]
@@ -84,6 +102,7 @@ options =
   , Option [] ["anf"]          (NoArg (Action desugarANFMain)) "desugar JavaScript into A-normal form"
   , Option [] ["no-env"]       (NoArg NoEnv)                   "exclude standard environment"
   , Option [] ["type-check"]   (ReqArg readChecker "CHECKER")  "run a type checker"
+  , Option [] ["ljs"]          (NoArg LJS)                     "parse lambda-js syntax"
   ]
 
 main :: IO ()
