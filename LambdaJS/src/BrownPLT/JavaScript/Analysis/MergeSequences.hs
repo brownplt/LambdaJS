c9b45edc2aad47dca606f4ca055293323d442d06
Spiridon Eliopoulos <seliopou@gmail.com>
2010-03-18 01:38:04 -0400

    [analysis] MergeSequence drops values when it can
    If a value is the first expression in a sequence, then it is safe to
    drop that expression. For example,
    
      (begin
        undefined
        (set! x 0))
    
    can be safely rewritten to
    
      (set! x 0).
    
    Also, dropped the 'c' in the type signatures in this module when not
    needed.

--- LambdaJS/src/BrownPLT/JavaScript/Analysis/MergeSequences.hs
+++ LambdaJS/src/BrownPLT/JavaScript/Analysis/MergeSequences.hs
@@ -1,11 +1,10 @@
-module BrownPLT.JavaScript.Analysis.MergeSequences
-    ( mergeSeqs ) where
+module BrownPLT.JavaScript.Analysis.MergeSequences ( mergeSeqs ) where
 
 import Data.Generics
 
-import BrownPLT.JavaScript.Semantics.Syntax
+import BrownPLT.JavaScript.Semantics.Syntax ( label )
 import BrownPLT.JavaScript.Semantics.ANF ( Value(..), BindExp(..), Exp(..) )
-import BrownPLT.JavaScript.Semantics.Prelude
+
 
 mergeSeqs :: forall a c. (Data a, Data (c a)) => c a -> c a
 mergeSeqs = everywhere' (mkT (mergeSeqsExp :: Exp a -> Exp a)
@@ -15,28 +14,27 @@ mergeSeqs = everywhere' (mkT (mergeSeqsExp :: Exp a -> Exp a)
 merge :: (Data a) => (Exp a -> Exp a) -> Exp a -> Exp a -> Exp a
 merge k e1 e2 =
     case e1 of
-      ALet a binds body -> merge (\e -> (k (ALet a binds e))) body e2
-      AReturn a v -> 
-          (k (ALet a [("$UNUSED", (BValue a v))] e2))
-      ABind a b ->
-          (k (ALet a [("$UNUSED", b)] e2))
-      otherwise -> do
-              ASeq (label e1) (k e1) e2
+      ALet a binds body -> merge (\e -> k (ALet a binds e)) body e2
+      AReturn a v -> k e2
+      -- NB: We should, whenever possible, ensure that the identifiers we introduce
+      -- are unique to the program.
+      ABind a b -> k (ALet a [("$UNUSED", b)] e2)
+      otherwise -> ASeq (label e1) (k e1) e2
 
-mergeSeqsExp :: forall a c. (Data a) => Exp a -> Exp a
+mergeSeqsExp :: forall a. (Data a) => Exp a -> Exp a
 mergeSeqsExp e =
     case e of
       ASeq a e1 e2 -> 
           merge id e1 e2
       otherwise -> e
 
-mergeSeqsVal :: forall a c. (Data a) => Value a -> Value a
+mergeSeqsVal :: forall a. (Data a) => Value a -> Value a
 mergeSeqsVal (VLambda a xs e) =
     (VLambda a xs (mergeSeqsExp e))
 mergeSeqsVal v = v
 
 
-mergeSeqsBind :: forall a c. (Data a) => BindExp a -> BindExp a
+mergeSeqsBind :: forall a. (Data a) => BindExp a -> BindExp a
 mergeSeqsBind b =
     gmapT (mkT (mergeSeqsExp :: Exp a -> (Exp a))
            `extT` (mergeSeqsVal :: Value a -> (Value a))
@@ -45,14 +43,12 @@ mergeSeqsBind b =
            `extT` (mergeSeqsValMap  
                    :: [(String, Value a)] -> [(String, Value a)])) b
 
-mergeSeqsValLst :: forall a c. (Data a) =>
-                   [Value a] -> [Value a]
+mergeSeqsValLst :: forall a. (Data a) => [Value a] -> [Value a]
 mergeSeqsValLst vs = map mergeSeqsVal vs
 
-mergeSeqsValMap :: forall a c. (Data a) => 
-                   [(String, Value a)] -> 
-                   [(String, Value a)]
+mergeSeqsValMap :: forall a. (Data a)
+                => [(String, Value a)]
+                -> [(String, Value a)]
 mergeSeqsValMap ps =
     map (\(s,v) -> (s, mergeSeqsVal v)) ps
 
-      
