brownplt/LambdaJS

commit cca29f0a524a611dc5a2855e72a7a1361fc12746

prefixOp in a function

LambdaJS/src/BrownPLT/JavaScript/Semantics/Desugar.hs
@@ -6,7 +6,7 @@ module BrownPLT.JavaScript.Semantics.Desugar
   , toString, toNumber, toObject, toBoolean
   , isNumber, isUndefined, isRefComb, isObject, isNull, isLocation, isPrim
   , isFunctionObj
-  , primToStr, primToNum, toPrimitive, strictEquality, abstractEquality
+  , primToStr, primToNum, toPrimitive, strictEquality
   , toPrimitive_Number
   , applyObj
   , eAnd, eNot, eOr, eNew, eNewDirect, eFor, eArgumentsObj
@@ -116,7 +116,6 @@ toPrimitive = toPrimitive_Number
 toNumber e = EApp nopos (EId nopos "@toNumber") [e]
 toBoolean = primToBool
 
-
 -- |Algorithm 9.8
 -- expects objects to be locations to be able to call toPrimitive.
 -- otherwise it should be a value.
@@ -127,48 +126,8 @@ toString e =
         (primToStr $ toPrimitive (EId nopos "$toStr"))
         (primToStr (EId nopos "$toStr"))
 
-
-abstractEquality :: ExprPos -> ExprPos -> ExprPos
-abstractEquality e1 e2 = ELet nopos [("$lhs", e1), ("$rhs", e2)] $
-  EIf nopos (EOp nopos OAbstractEq [EId nopos "$lhs", EId nopos "$rhs"]) 
-      (EBool nopos True) $
-  EIf nopos (isLocation (EId nopos "$lhs"))
-      (EOp nopos OAbstractEq [toPrimitive (EId nopos "$lhs"), EId nopos "$rhs"]) $
-  EIf nopos (isLocation (EId nopos "$rhs"))
-      (EOp nopos OAbstractEq [EId nopos "$lhs", toPrimitive (EId nopos "$rhs")])
-      (EBool nopos False)
-
-
-
-      
-infixOp :: InfixOp -> ExprPos -> ExprPos -> ExprPos
 infixOp op e1 e2 = EApp nopos (EId nopos ("@" ++ show op)) [e1, e2]
-
-prefixOp :: PrefixOp -> ExprPos -> ExprPos
-prefixOp op e = case op of 
-  -- It is strange that that the subterm of delete is an expression and not
-  -- an l-value.  Note that that delete has no effect when its subexpression
-  -- is not l-value like.
-  PrefixDelete -> case e of
-    EGetField a1 (EDeref a2 eObj) eString ->
-      ELet nopos [("$delObj", eObj),
-            ("$delStr", eString)] $
-        EIf nopos (EOp nopos OObjCanDelete [EDeref nopos $ EId nopos "$delObj", EId nopos "$delStr"])
-            (ESeq nopos
-              (ESetRef nopos (EId nopos "$delObj")
-                (EDeleteField a1 (EDeref a2 $ EId nopos "$delObj") (EId nopos "$delStr")))
-              (EBool nopos True))
-            (EBool nopos False)
-    otherwise -> EBool nopos True
-    
-  PrefixVoid -> ESeq nopos (getValue e) (EUndefined nopos)
-  --TODO: make sure step 3 in 11.4.3 makes sense for our typeof:
-  PrefixTypeof -> EOp nopos OSurfaceTypeof [getValue e]
-  PrefixBNot -> EOp nopos OBNot [EOp nopos OToInt32 [toNumber e]]
-  PrefixLNot -> eNot (toBoolean e)
-  PrefixPlus -> toNumber e
-  PrefixMinus -> EOp nopos OSub [ENumber nopos 0.0, toNumber e]
-
+prefixOp op e = EApp nopos (EId nopos ("@" ++ show op)) [e]
 
 type Env = M.Map Ident Bool
 

LambdaJS/src/BrownPLT/JavaScript/Semantics/ECMAEnvironment.hs
@@ -5,7 +5,7 @@ module BrownPLT.JavaScript.Semantics.ECMAEnvironment
 import BrownPLT.JavaScript.Semantics.Desugar
 import BrownPLT.JavaScript.Semantics.Syntax
 import Text.ParserCombinators.Parsec
-import BrownPLT.JavaScript.Syntax (InfixOp (..))
+import BrownPLT.JavaScript.Syntax (InfixOp (..), PrefixOp (..))
 import BrownPLT.JavaScript.Parser (parseExpression)
 import qualified Data.Map as M
 
@@ -146,11 +146,50 @@ infixOp' op e1 e2 = case op of
         ELet nopos [("$bitLhs", EOp nopos OToInt32 [toNumber (EId nopos "$opLhs")]),
               ("$bitRhs", EOp nopos OToInt32 [toNumber (EId nopos "$opRhs")])] $
           EOp nopos eop [EId nopos "$bitLhs", EId nopos "$bitRhs"]
-
+    abstractEquality e1 e2 = ELet nopos [("$lhs", e1), ("$rhs", e2)] $
+      EIf nopos (EOp nopos OAbstractEq [EId nopos "$lhs", EId nopos "$rhs"]) 
+          (EBool nopos True) $
+      EIf nopos (isLocation (EId nopos "$lhs"))
+          (EOp nopos OAbstractEq [toPrimitive (EId nopos "$lhs"),
+                                  EId nopos "$rhs"]) $
+      EIf nopos (isLocation (EId nopos "$rhs"))
+          (EOp nopos OAbstractEq [EId nopos "$lhs", 
+                                  toPrimitive (EId nopos "$rhs")])
+          (EBool nopos False)
+    
 makeInfixOp op = 
   ("@" ++ show op,
    ELambda nopos ["@x", "@y"] (infixOp' op (EId nopos "@x") (EId nopos "@y")))
-                      
+
+prefixOp' :: PrefixOp -> ExprPos -> ExprPos
+prefixOp' op e = case op of 
+  -- It is strange that that the subterm of delete is an expression and not
+  -- an l-value.  Note that that delete has no effect when its subexpression
+  -- is not l-value like.
+  PrefixDelete -> case e of
+    EGetField a1 (EDeref a2 eObj) eString ->
+      ELet nopos [("$delObj", eObj),
+            ("$delStr", eString)] $
+        EIf nopos (EOp nopos OObjCanDelete [EDeref nopos $ EId nopos "$delObj", EId nopos "$delStr"])
+            (ESeq nopos
+              (ESetRef nopos (EId nopos "$delObj")
+                (EDeleteField a1 (EDeref a2 $ EId nopos "$delObj") (EId nopos "$delStr")))
+              (EBool nopos True))
+            (EBool nopos False)
+    otherwise -> EBool nopos True
+    
+  PrefixVoid -> ESeq nopos (getValue e) (EUndefined nopos)
+  --TODO: make sure step 3 in 11.4.3 makes sense for our typeof:
+  PrefixTypeof -> EOp nopos OSurfaceTypeof [getValue e]
+  PrefixBNot -> EOp nopos OBNot [EOp nopos OToInt32 [toNumber e]]
+  PrefixLNot -> eNot (toBoolean e)
+  PrefixPlus -> toNumber e
+  PrefixMinus -> EOp nopos OSub [ENumber nopos 0.0, toNumber e]
+         
+makePrefixOp op =
+  ("@" ++ show op,
+   ELambda nopos ["@x"] (prefixOp' op (EId nopos "@x")))
+             
 
 -- |According to the specification, toPrimitive may signal a TypeError.
 -- this is generalized to do either toString first, or valueOf first,
@@ -1024,6 +1063,13 @@ ecma262Env body =
   ELet nopos [makeInfixOp OpBXor] $
   ELet nopos [makeInfixOp OpBOr] $
   ELet nopos [makeInfixOp OpAdd] $
+  ELet nopos [makePrefixOp PrefixLNot] $
+  ELet nopos [makePrefixOp PrefixBNot] $
+  ELet nopos [makePrefixOp PrefixPlus] $
+  ELet nopos [makePrefixOp PrefixMinus] $
+  ELet nopos [makePrefixOp PrefixTypeof] $
+  ELet nopos [makePrefixOp PrefixVoid] $
+  ELet nopos [makePrefixOp PrefixDelete] $
   updateObject (EId nopos "@Object_prototype") objectPrototypeValues $
   updateObject (EId nopos "@Function_prototype") functionPrototypeValues $
   ELet nopos [("$Date.prototype", ERef nopos datePrototype)] $

commit e4fd79933434d471bd06f67a2a27f8dbd49b1808

fixed delete-operator regression in cca29f0a524a611dc5a2855e72a7a1361fc12746

LambdaJS/run.sh
@@ -5,4 +5,4 @@ then
   exit 65
 fi
 
-./dist/build/lambdaJS/lambdaJS --desugar < $1 | ../Redex/interp-shell.ss
+./dist/build/lambdaJS/lambdaJS --desugar --env es3.env < $1 | ../Redex/interp-shell.ss

LambdaJS/src/BrownPLT/JavaScript/Semantics/Desugar.hs
@@ -128,7 +128,12 @@ toString e =
 
 infixOp op e1 e2 = EApp nopos (EId nopos ("@" ++ show op)) [e1, e2]
 
-prefixOp op e = EApp nopos (EId nopos ("@" ++ show op)) [e]
+prefixOp op e = case op of
+  PrefixDelete -> case e of
+    EGetField a1 (EDeref a2 eObj) eString ->
+      EApp nopos (EId nopos "@delete") [eObj, eString]
+    otherwise -> EBool nopos True
+  otherwise -> EApp nopos (EId nopos ("@" ++ show op)) [e]
 
 type Env = M.Map Ident Bool
 

LambdaJS/src/BrownPLT/JavaScript/Semantics/ECMAEnvironment.hs
@@ -190,23 +190,23 @@ makeInfixOp op =
   ("@" ++ show op,
    ELambda nopos ["@x", "@y"] (infixOp' op (EId nopos "@x") (EId nopos "@y")))
 
+
+-- It is strange that that the subterm of delete is an expression and not
+-- an l-value.  Note that that delete has no effect when its subexpression
+-- is not l-value like.
+delete = ELambda nopos ["$delObj", "$delStr"] $
+  EIf nopos (EOp nopos OObjCanDelete [EDeref nopos $ EId nopos "$delObj", 
+             EId nopos "$delStr"])
+      (ESeq nopos
+        (ESetRef nopos (EId nopos "$delObj")
+          (EDeleteField nopos (EDeref nopos $ EId nopos "$delObj") 
+                           (EId nopos "$delStr")))
+        (EBool nopos True))
+      (EBool nopos False)
+
 prefixOp' :: PrefixOp -> ExprPos -> ExprPos
 prefixOp' op e = case op of 
-  -- It is strange that that the subterm of delete is an expression and not
-  -- an l-value.  Note that that delete has no effect when its subexpression
-  -- is not l-value like.
-  PrefixDelete -> case e of
-    EGetField a1 (EDeref a2 eObj) eString ->
-      ELet nopos [("$delObj", eObj),
-            ("$delStr", eString)] $
-        EIf nopos (EOp nopos OObjCanDelete [EDeref nopos $ EId nopos "$delObj", EId nopos "$delStr"])
-            (ESeq nopos
-              (ESetRef nopos (EId nopos "$delObj")
-                (EDeleteField a1 (EDeref a2 $ EId nopos "$delObj") (EId nopos "$delStr")))
-              (EBool nopos True))
-            (EBool nopos False)
-    otherwise -> EBool nopos True
-    
+  PrefixDelete -> error "Delete operator treated specially"
   PrefixVoid -> ESeq nopos (getValue e) (EUndefined nopos)
   --TODO: make sure step 3 in 11.4.3 makes sense for our typeof:
   PrefixTypeof -> EOp nopos OSurfaceTypeof [getValue e]
@@ -1098,7 +1098,7 @@ ecma262Env body =
   ELet nopos [makePrefixOp PrefixMinus] $
   ELet nopos [makePrefixOp PrefixTypeof] $
   ELet nopos [makePrefixOp PrefixVoid] $
-  ELet nopos [makePrefixOp PrefixDelete] $
+  ELet nopos [("@delete", delete)] $
   ELet nopos [("@setArray", setArray)] $
   updateObject (EId nopos "@Object_prototype") objectPrototypeValues $
   updateObject (EId nopos "@Function_prototype") functionPrototypeValues $